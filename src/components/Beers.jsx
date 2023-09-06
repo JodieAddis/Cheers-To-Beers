60ad3c49d7c82a8701233cf9f8e432460d171537
JodieAddis <jodie.addis.92@gmail.com>
2023-09-06 14:37:02 +0200

    Using an MUI autocomplete to display beer names.

--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -1,16 +1,11 @@
 import React from "react";
+import { SearchBeers } from "./SearchBeers";
 
 export const Beers = () => {
     return (
         <div>
             <section>
-                <select name="" id="">
-                    <option value="beer 1">Beer 1</option>
-                    <option value="beer 2">Beer 2</option>
-                </select>
-            </section>
-            <section>
-                {/* Faire un map qui passe sur l'API pour afficher toutes les bières */}
+                <SearchBeers />
             </section>
         </div>
     );
--- src/components/SearchBeers.jsx
+++ src/components/SearchBeers.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { TextField } from "@mui/material";
+import { Stack } from "@mui/material";
+import { Autocomplete } from "@mui/material";
+import { Box } from "@mui/material";
+import { width } from "@mui/system";
 
 export const SearchBeers = () => {
-    return <div>SearchBeers</div>;
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.punkapi.com/v2/beers")
+            .then((response) => response.json())
+            .then((json) => {
+                setData(json);
+            });
+    }, []);
+
+    // console.log(data);
+    return (
+        <Stack sx={{ width: 200, height: 50 }}>
+            <Autocomplete
+                id="beer_id"
+                getOptionLabel={(data) => `${data.name}`} //Ce sont les noms des bière qui seront affichés dans le select
+                options={data}
+                sx={{ width: 200, height: 50 }}
+                isOptionEqualToValue={(options, value) =>
+                    options.name === value.name
+                }
+                noOptionsText={"No avaiblable beer"}
+                renderOption={(props, data) => (
+                    <Box component="li" {...props} key={data.id}>
+                        {data.name}
+                    </Box>
+                )}
+                renderInput={(params) => (
+                    <TextField {...params} label="Search for a Beer" />
+                )}
+            />
+        </Stack>
+    );
 };
