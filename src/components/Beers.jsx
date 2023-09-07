7e2739977ea27e4d5f84cc3559e3779b3b3176dd
JodieAddis <jodie.addis.92@gmail.com>
2023-09-07 10:21:43 +0200

    Personnalized hook useFetch to fetch data. Display all beer (name, abv and tagline). Tailwind for the structure of every card of beer.

--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchBeers } from "./SearchBeers";
+import { useFetch } from "../hooks/useFetch";
 
-export const Beers = () => {
+import { IconContext } from "react-icons";
+import { IoMdBeer } from "react-icons/io";
+import { Icon } from "@mui/material";
+
+export const Beers = ({ BeerName }) => {
+    const [beerSelected, setBeerSelected] = useState(false); //Etatt pour savoir quoi affiché ou alors faire une nouvelle page ?
+
+    const { data, isPending, error } = useFetch(
+        "https://api.punkapi.com/v2/beers"
+    );
     return (
         <div>
-            <section className="mt-20">
+            <section className="flex justify-center mt-20">
                 <SearchBeers />
+            </section>
+            <section className="mt-12">
+                {data &&
+                    data.map((beer, id) => (
+                        <div className="flex justify-center sm: flex-row">
+                            <div className="justify-center my-6 w-64 border-black border-solid border-2 rounded-lg">
+                                <div className="flex justify-center">
+                                    <img
+                                        key={id}
+                                        src={beer.image_url}
+                                        alt="`Picture of the beer ${beer.name}`"
+                                        className="w-12 mt-6"
+                                    />
+                                </div>
+                                <div className="flex justify-between mt-6 mx-6">
+                                    <p key={id} className="font-bold w-28">
+                                        {beer.name}
+                                    </p>
+                                    <div className="flex flex-row">
+                                        <IconContext.Provider
+                                            value={{
+                                                size: "1.5em",
+                                            }}
+                                        >
+                                            <IoMdBeer />
+                                        </IconContext.Provider>
+                                        <p key={id} className="ml-2">
+                                            {beer.abv}°
+                                        </p>
+                                    </div>
+                                </div>
+                                <p className="text-center my-5">
+                                    {beer.tagline}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
             </section>
         </div>
     );
--- src/components/SearchBeers.jsx
+++ src/components/SearchBeers.jsx
@@ -1,43 +1,43 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { TextField } from "@mui/material";
-import { Stack } from "@mui/material";
-import { Autocomplete } from "@mui/material";
-import { Box } from "@mui/material";
-import { width, height } from "@mui/system";
+import React, { useState, useEffect } from "react";
+import { TextField, Stack, Autocomplete } from "@mui/material";
+import { AllBeers } from "../pages/AllBeers";
+import { useFetch } from "../hooks/useFetch";
 
 export const SearchBeers = () => {
-    const [data, setData] = useState([]);
+    const [selectedBeer, setSelectedBeer] = useState(null);
 
-    useEffect(() => {
-        fetch("https://api.punkapi.com/v2/beers")
-            .then((response) => response.json())
-            .then((json) => {
-                setData(json);
-            });
-    }, []);
+    const { data, isPending, error } = useFetch(
+        "https://api.punkapi.com/v2/beers"
+    );
 
-    // console.log(data);
+    const handleBeerSelected = (event, value) => {
+        setSelectedBeer(value);
+    };
+
     return (
-        <Stack sx={{ width: 200, height: 50 }}>
-            <Autocomplete
-                id="beer_id"
-                getOptionLabel={(data) => `${data.name}`} //Ce sont les noms des bière qui seront affichés dans le select
-                options={data}
-                sx={{ width: 200, height: 50 }}
-                isOptionEqualToValue={(options, value) =>
-                    options.name === value.name
-                }
-                noOptionsText={"No avaiblable beer"}
-                renderOption={(props, data) => (
-                    <Box component="li" {...props} key={data.id}>
-                        {data.name}
-                    </Box>
-                )}
-                renderInput={(params) => (
-                    <TextField {...params} label="Search for a Beer" />
-                )}
-            />
-        </Stack>
+        <>
+            <Stack sx={{ width: 200, height: 50 }}>
+                <Autocomplete
+                    id="beer_id"
+                    getOptionLabel={(option) => option.name}
+                    options={data}
+                    sx={{ width: 200, height: 50 }}
+                    isOptionEqualToValue={(option, value) =>
+                        option.name === value.name
+                    }
+                    noOptionsText="No available beer"
+                    renderOption={(props, option) => (
+                        <li {...props}>{option.name}</li>
+                    )}
+                    renderInput={(params) => (
+                        <TextField {...params} label="Search for a Beer" />
+                    )}
+                    onChange={handleBeerSelected}
+                    value={selectedBeer}
+                />
+            </Stack>
+            <p>{selectedBeer ? selectedBeer.name : null}</p>
+            {/* <AllBeers BeerName={selectedBeer ? selectedBeer.name : null} /> */}
+        </>
     );
 };
